80aaf4630ed711c3804af777ca8412e960d7ab1a
victorbu <victorbu.xx@gmail.com>
2019-03-15 07:46:56 +0800

    import spring boot [Building an Application with Spring Boot](https://spring.io/guides/gs/spring-boot/)

--- src/main/java/com/karonda/App.java
+++ src/main/java/com/karonda/App.java
@@ -1,13 +1,27 @@
 package com.karonda;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@EnableAutoConfiguration
+@RestController
 public class App 
 {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        SpringApplication.run(App.class, args);
     }
 }
