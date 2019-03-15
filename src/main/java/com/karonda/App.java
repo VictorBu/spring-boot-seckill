225118f875ae00456e925c43ed9715c3e2fabb38
victorbu <victorbu.xx@gmail.com>
2019-03-15 17:42:30 +0800

    mybatis, mybatis-generator

--- src/main/java/com/karonda/App.java
+++ src/main/java/com/karonda/App.java
@@ -1,7 +1,11 @@
 package com.karonda;
 
+import com.karonda.dao.UserDOMapper;
+import com.karonda.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.karonda"})
 @RestController
-public class App 
-{
+@MapperScan("com.karonda.dao")
+public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "Hello World!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户对象不存在";
+        }else{
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args )
