9f14700bf99a64fcbb1cd3f13ebfcdf4273b2d09
victorbu <victorbu.xx@gmail.com>
2019-03-20 14:30:10 +0800

    validator

--- src/main/java/com/karonda/service/Impl/UserServiceImpl.java
+++ src/main/java/com/karonda/service/Impl/UserServiceImpl.java
@@ -8,6 +8,8 @@ import com.karonda.error.BusinessException;
 import com.karonda.error.EmBusinessError;
 import com.karonda.service.UserService;
 import com.karonda.service.model.UserModel;
+import com.karonda.validator.ValidationResult;
+import com.karonda.validator.ValidatorImpl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserPasswordDOMapper userPasswordDOMapper;
+
+    @Autowired
+    private ValidatorImpl validator;
 
     @Override
     public UserModel getUserById(Integer id){
@@ -45,12 +50,9 @@ public class UserServiceImpl implements UserService {
             throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
         }
 
-        if(StringUtils.isEmpty(userModel.getTelphone())
-                || StringUtils.isEmpty(userModel.getName())
-                || userModel.getGender() == null
-                || userModel.getAge() == null
-                || StringUtils.isEmpty(userModel.getEncrptPassword())){
-            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
+        ValidationResult result = validator.validate(userModel);
+        if(result.isHasErrors()){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, result.getErrMsg());
         }
 
         UserDO userDO = convertFromModel(userModel);
--- src/main/java/com/karonda/service/model/UserModel.java
+++ src/main/java/com/karonda/service/model/UserModel.java
@@ -1,15 +1,27 @@
 package com.karonda.service.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class UserModel {
 
     private Integer id;
+    @NotBlank(message = "用户名不能为空")
     private String name;
+    @NotNull(message = "性别不能不填写")
     private Byte gender;
+    @NotNull(message = "年龄不能不填写")
+    @Min(value = 0, message = "年龄必须大于0")
+    @Max(value = 150, message = "年龄必须小于 150 岁")
     private Integer age;
+    @NotBlank(message = "手机号不能为空")
     private String telphone;
     private String registerMode;
     private String thirdPartyId;
 
+    @NotBlank(message = "密码不能为空")
     private String encrptPassword;
 
     public Integer getId() {
